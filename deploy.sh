c69c7bc5f38f730a4fdb5b553c64aead013bd036
majdabbas <majd.abbas999@gmail.com>
2025-05-14 10:55:01 +0300

    adding yolo service deploy

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+path_to_file=$1
+copy the .servcie file
+sudo cp yolo.service /etc/systemd/system/
+
+# reload daemon and restart the service
+sudo systemctl daemon-reload
+sudo systemctl restart yolo.service
+sudo systemctl enable yolo.service
+sudo systemctl start yolo.service
+if ! systemctl is-active --quiet yolo.service; then
+  echo "❌ yolo.service is not running."
+  sudo systemctl status yolo.service --no-pager
+  exit 1
+fi
+
+# Check if the virtual environment exists
+if [ ! -d "$path_to_file/.venv" ]; then  # Check if .venv is a directory
+    python3 -m venv "$path_to_file/.venv"
+    "$path_to_file/.venv/bin/pip" install -r "$path_to_file/torch-requirements.txt"
+    "$path_to_file/.venv/bin/pip" install -r "$path_to_file/requirements.txt"
+else
+    echo "Virtual environment already exists."
+fi
