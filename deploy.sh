aa0facd56eeef025de6533c62a6122982b5d93d6
majdabbas <majd.abbas999@gmail.com>
2025-05-28 19:52:49 +0300

    adding dev

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 path_to_file=$1
+s3_bucket_name_dev=$2
 copy the .servcie file
 sudo cp yolo.service /etc/systemd/system/
 
@@ -15,6 +16,7 @@ if ! systemctl is-active --quiet yolo.service; then
 fi
 
 # Check if the virtual environment exists
+echo "S3_BUCKET_NAME=$s3_bucket_name_dev" > $path_to_file/.env
 if [ ! -d "$path_to_file/.venv" ]; then  # Check if .venv is a directory
     python3 -m venv "$path_to_file/.venv"
     "$path_to_file/.venv/bin/pip" install -r "$path_to_file/torch-requirements.txt"
