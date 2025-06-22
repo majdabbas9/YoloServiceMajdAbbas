48ede0e2344ccc64e36a79682fc8b7ac5cdc64fb
majdabbas <majd.abbas999@gmail.com>
2025-06-22 21:49:09 +0300

    adding dynamodb

--- Test/test_api.py
+++ Test/test_api.py
@@ -1,28 +1,20 @@
 import os
 import shutil
 import sqlite3
-import uuid
 import pytest
-
-from unittest.mock import patch, MagicMock
+from db_for_prediction import DatabaseFactory
 from fastapi.testclient import TestClient
-from app import app, UPLOAD_DIR, PREDICTED_DIR, DB_PATH, db
-
+from app import app
 client = TestClient(app)
 
-TEST_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "test_image.jpg")
-INVALID_IMAGE_PATH = "invalid_file.txt"
-DUMMY_UIDS = ["1","2", "3"]
-DUMMY_ORIGINAL_IMAGES = ["original_image1.jpg", "original_image2.jpg", "original_image3.jpg"]
-DUMMY_PREDICTED_IMAGES = ["predicted_image1.jpg", "predicted_image2.jpg", "predicted_image3.jpg"]
-DUMMY_LABELS = ["cat","dog", "bird"]
-DUMMY_SCORES = ["0.95", "0.85", "0.75"]
-DUMMY_BOXS = [[10.0, 20.0, 30.0, 40.0], [15.0, 25.0, 35.0, 45.0], [5.0, 10.0, 15.0, 20.0]]
 
-
-# Clean the DB and folders before/after all tests
+#Clean The DB
 @pytest.fixture(scope="module", autouse=True)
 def setup_and_teardown():
+
+    UPLOAD_DIR = "uploads/original"
+    PREDICTED_DIR = "uploads/predicted"
+    DB_PATH = "predictions.db"
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
     if os.path.exists(UPLOAD_DIR):
@@ -33,7 +25,13 @@ def setup_and_teardown():
     os.makedirs(UPLOAD_DIR, exist_ok=True)
     os.makedirs(PREDICTED_DIR, exist_ok=True)
 
-    yield
+    db = DatabaseFactory.create_database("sqlite", db_path=DB_PATH)
+    DUMMY_UIDS = ["1", "2", "3"]
+    DUMMY_ORIGINAL_IMAGES = ["original_image1.jpg", "original_image2.jpg", "original_image3.jpg"]
+    DUMMY_PREDICTED_IMAGES = ["predicted_image1.jpg", "predicted_image2.jpg", "predicted_image3.jpg"]
+    DUMMY_LABELS = ["cat", "dog", "bird"]
+    DUMMY_SCORES = ["0.95", "0.85", "0.75"]
+    DUMMY_BOXS = [[10.0, 20.0, 30.0, 40.0], [15.0, 25.0, 35.0, 45.0], [5.0, 10.0, 15.0, 20.0]]
     for i in range(len(DUMMY_UIDS)):
         db.save_prediction_session(
             uid=DUMMY_UIDS[i],
@@ -47,6 +45,7 @@ def setup_and_teardown():
             score=DUMMY_SCORES[i],
             box=DUMMY_BOXS[i]
         )
+    yield
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
     if os.path.exists(UPLOAD_DIR):
@@ -60,27 +59,15 @@ def test_health():
     assert resp.status_code == 200
     assert resp.json() == {"status": "ok", "message": "Service is running"}
 
-def test_predict_invalid_file_type():
-    with open(INVALID_IMAGE_PATH, "w") as f:
-        f.write("this is not an image")
-
-    try:
-        with open(INVALID_IMAGE_PATH, "rb") as f:
-            # NOTE: this is not valid since /predict does not accept files
-            resp = client.post("/predict", files={"file": ("invalid_file.txt", f, "text/plain")})
-            assert resp.status_code != 200
-    finally:
-        os.remove(INVALID_IMAGE_PATH)
-
 def test_prediction_by_uid():
     resp = client.get("/prediction/1")
     assert resp.status_code == 200
-    assert resp.json().get('uid')  == 1
+    assert resp.json().get("uid") == "1"
 
 def test_prediction_by_correct_label():
     resp = client.get("/predictions/label/bird")
     assert resp.status_code == 200
-    assert resp.json().get('uid') == 3
+    assert resp.json()[0].get("uid") == "3"
 
 def test_prediction_by_wrong_label():
     resp = client.get("/predictions/label/dogg")
@@ -90,5 +77,7 @@ def test_prediction_by_wrong_label():
 def test_prediction_by_score():
     resp = client.get("/predictions/score/0.8")
     assert resp.status_code == 200
-    assert resp.json() == [{'uid': '1'}, {'uid': '2'}]
+    assert resp.json() == [{'uid': '1'}, {'uid': '2'}] or resp.json() == [{'uid': '2'}, {'uid': '1'}]
 
+
+#Check This Test again
